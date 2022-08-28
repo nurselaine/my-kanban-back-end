ebd11da581553639ef08f8c9a86426c19a9a47df
nurselaine <Elaine.t.huynh@gmail.com>
2022-08-27 20:18:29 -0700

    connected server to firebase and added endpoints to add new tasks

--- modules/firestore.js
+++ modules/firestore.js
@@ -7,23 +7,59 @@ app.use(cors());
 app.use(express.json());
 
 const admin = require('firebase-admin');
-const credential = {
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY,
-}
+const { response } = require('express');
+// const credential = {
+//   projectId: process.env.FIREBASE_PROJECT_ID,
+//   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+//   privateKey: process.env.FIREBASE_PRIVATE_KEY,
+// }
 
 admin.initializeApp({
-  credential: admin.credential.cert({ credential }),
+  credential: admin.credential.cert({  
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY, }),
   databaseURL: process.env.FIREBASE_DATABASE_URL,
 });
 
+const db = admin.firestore();
+app.use(express.urlencoded({ extended: true }));
 const firestore = {};
 
+// add to task collection
 firestore.addTask = async (req, res) => {
   try{
+    const task = {
+      flare: req.body.flare,
+      title: req.body.title,
+      details: req.body.details,
+      status: req.body.status
+    }
+    const response = await db.collection('tasks').add(task);
+    res.status(200).send(task);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+}
 
+// delete selected task
+firestore.deleteTask = async (req, res) => {
+  try{
+    const id = req.body.id;
+    const reponseRef = await db.collection('tasks').doc(id).delete();
+    req.status(200).send('task deleted');
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
+}
+
+// get all tasks
+firestore.getAllTasks = async (req, res) => {
+  try {
+    const response = db.collection('tasks')
   } catch (error) {
 
   }
 }
+
+module.exports = firestore;
--- server.js
+++ server.js
@@ -3,9 +3,29 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const { request } = require('express');
 require('dotenv').config();
 const PORT = process.env.PORT || 3002;
+const firestore = require('./modules/firestore');
 
 
 app.use(express.json());
 app.use(cors());
+
+app.get('/', res => {
+  try{
+    console.log('Kanban Server');
+    res.status(200).send('Welcome to my Kanban server!');
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
+});
+
+// Database requests
+app.post('/addTask', firestore.addTask);
+
+app.get('*', res => {
+    res.send('Nothing is here! Try another endpoint...');
+});
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
